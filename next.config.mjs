4fa7b089d27480b56e1a9cdd0cdd654018517078
Nived Shaji <83071573+Nivet2006@users.noreply.github.com>
2026-06-09 19:15:11 +0530

    fix: add X-Content-Type-Options nosniff header for all routes in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -41,6 +41,19 @@ const nextConfig = {
             },
         ],
     },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                ],
+            },
+        ];
+    },
 }
 
 export default nextConfig
